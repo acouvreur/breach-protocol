833f9697c5c82ce3264c2f9a811bf324f1a475fd
jasupralog <japuskaric@supralog.com>
2020-12-22 10:31:54 +0100

    Miam les bons desserts

--- src/components/Sequence/Sequence.jsx
+++ src/components/Sequence/Sequence.jsx
@@ -7,18 +7,43 @@ import { Cell } from '../_shared';
 const StyledAside = styled.aside`
   min-height: 52px;
   min-width: ${(props) => `calc(60px * ${props.size})`};
+  background-color: ${(props) => `${props.color}`};
 `;
+
+const sequenceIsInBuffer = (buffer, sequence) => 
+  buffer.values.toString().includes(sequence.toString());
+
+const sequenceCanBeInBuffer = (buffer, sequence) => {
+  const remainingSpace = buffer.capacity - buffer.values.length;
+  if (remainingSpace < sequence.length) {
+    for (let i = remainingSpace; i < sequence.length; i++) {
+      const startOfSequenceNeeded = sequence.slice(0,sequence.length - i).toString();
+      if (buffer.values.toString().endsWith(startOfSequenceNeeded)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  return true;
+}
 
 /**
  *
  * @param {Object} props
  * @param {number} props.sequence
  */
-const Sequence = ({ sequence }) => {
-  const { setHighlightedSymbol } = useContext(HighlightedSymbolContext);
+const Sequence = ({sequence, buffer}) => {
+  const {setHighlightedSymbol} = useContext(HighlightedSymbolContext);
+
+  let color = 'black';
+  if (sequenceIsInBuffer(buffer, sequence)) {
+    color= 'green';
+  } else if (!sequenceCanBeInBuffer(buffer, sequence)) {
+    color = 'red';
+  }
 
   return (
-    <StyledAside>
+    <StyledAside color={color}>
       {sequence.map((symbol, i) => {
         return (
           <Cell
